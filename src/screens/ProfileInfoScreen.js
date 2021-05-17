78ca94258b4b843dfae2bbb76f6f0b628ecca732
cguti8 <infocesarguti@gmail.com>
2021-05-17 20:04:53 +0200

    profileScreenStructure

---
+++ src/screens/ProfileInfoScreen.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import colors from '../constants/Colors';
+
+function ProfileInfoScreen() {
+  return <View></View>;
+}
