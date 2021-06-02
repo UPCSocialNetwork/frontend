a41165f1f2baa1218ec0fb217aed428b3cb286b9
cguti8 <infocesarguti@gmail.com>
2021-06-02 18:53:27 +0200

    first front finished

--- src/screens/RegisterMentor1Screen.js
+++ src/screens/RegisterMentor1Screen.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Text, TouchableOpacity, FlatList } from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity, Modal, FlatList } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 import BaseButton from '../components/BaseButton';
 import BackHeader from '../components/BackHeader';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { MaterialIcons, SimpleLineIcons } from '@expo/vector-icons';
 import { useFonts } from 'expo-font';
 
 import axios from '../constants/axios';
@@ -72,8 +73,11 @@ export default function RegisterMentor1Screen({ navigation }) {
           navigation.goBack();
         }}
       ></BackHeader>
-      <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center' }}>
-        <View style={styles.headerContainer}></View>
+      <View>
+        <View style={styles.headerContainer}>
+          <Text style={styles.textHeader}>Vols ser mentoritzat?</Text>
+          <Text style={styles.textHeader}>Selecciona el teu mentor!</Text>
+        </View>
         <View style={styles.mainContainer}>
           <View style={styles.textErrorInputs}>
             {!errorText.errorStatus ? null : (
@@ -83,10 +87,24 @@ export default function RegisterMentor1Screen({ navigation }) {
             )}
           </View>
         </View>
-        <View style={styles.registerButton}>
+        <View style={styles.searchBloc}>
+          <TouchableOpacity style={styles.dropdown}>
+            <View style={styles.dropdownIcon}></View>
+            <View style={styles.dropdownTextContainer}>
+              <Text style={styles.dropdownText} numberOfLines={1} ellipsizeMode="tail">
+                Cerca el teu mentor...
+              </Text>
+            </View>
+            <View style={{ backgroundColor: Colors.black, height: '86%', width: 1 }}></View>
+            <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-end' }}>
+              <MaterialIcons style={{ marginEnd: 17 }} name="search" size={25} color={Colors.secondary} />
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.registerButton1}>
           <BaseButton onPress={registerMentorHandler} title="Següent" btnColor={Colors.primary} />
         </View>
-        <View style={styles.registerButton}>
+        <View style={styles.registerButton2}>
           <BaseButton onPress={registerMentorHandler} title="No vull ser mentoritzat" btnColor={Colors.red} />
         </View>
       </View>
@@ -100,12 +118,12 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   headerContainer: {
-    flexDirection: 'row',
-    alignContent: 'center',
-    alignItems: 'center',
-    marginTop: 10,
-    height: 55,
-    width: Window.width * 0.9,
+    marginTop: Window.height * 0.1,
+  },
+  textHeader: {
+    fontSize: 18,
+    fontFamily: 'InterSemiBold',
+    alignSelf: 'center',
   },
   mainContainer: {
     flex: 1,
@@ -122,7 +140,40 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     color: Colors.red,
   },
-  registerButton: {
+  dropdown: {
+    alignSelf: 'center',
+    width: Window.width * 0.85,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    height: 55,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: Colors.lightBlack,
+    marginTop: 30,
+  },
+  dropdownIcon: {
+    width: 18,
+    height: 18,
+    borderRadius: 50,
+    marginLeft: 18,
+    backgroundColor: Colors.darkBlue,
+  },
+  dropdownTextContainer: {
+    width: Window.width * 0.61,
+  },
+  dropdownText: {
+    fontFamily: 'InterMedium',
+    marginRight: 15,
+    marginLeft: 15,
+    fontSize: 14,
+  },
+  registerButton1: {
+    marginBottom: 20,
+    alignItems: 'center',
+    marginTop: Window.height * 0.3,
+  },
+  registerButton2: {
     marginBottom: 20,
     alignItems: 'center',
   },
